1066ba919b1393b31fc25390d754071832a408d5
Ruchi <ruchi.chudasama@cern.ch>
2024-12-10 03:56:38 -0600

    cut and count added for pyroot histograms

--- ntupleAnalysis/cutNcount.py
+++ ntupleAnalysis/cutNcount.py
@@ -52,45 +52,77 @@ def loadlegend(top, bottom, left, right):
     legend.SetBorderSize(0)
     return legend
 
-histos={}
+def makeFileList(path):
+    files = os.listdir(path)
+    root_files = [path+f for f in files if f.endswith(".root")]
+    return root_files
+    #return root_files
 
-files_ = []
-firstfile = True
-fileList = []
-for roots,dirs,files in os.walk('/eos/user/r/rchudasa/e2e_project/Tau_data_ntuples/'):
-    for name in files:
-        file = os.path.join(roots,name)
-        fileList.append(file)
+commonMCPath = '/eos/uscms/store/group/lpcml/rchudasa/MCGenerationRun3/'
+# tau_3p7= commonMCPath+"HToAATo4Tau_hadronic_tauDecay_M3p7_Run3_2023/3p7_MLAnalyzer_ntuples_v1/241209_155112/0000/"
+# tau_3p7fList = makeFileList(tau_3p7)
+# fileList = tau_3p7fList[0:2]
+
         
 #print(fileList)
 
-for path in range(len(fileList)):
-    print(path, fileList[path])
-    files_.append( TFile.Open(fileList[path]) )
-    tmp_hlt = files_[-1].Get('fevt/hNpassed_hlt')
-    tmp_kin = files_[-1].Get('fevt/hNpassed_kin')
-    tmp_mva = files_[-1].Get('fevt/hNpassed_MVA')
-    tmp_mass= files_[-1].Get('fevt/hNpassed_mGG')
-    tmp_sel = files_[-1].Get('fevt/h_sel')
-    print(path, tmp_sel.GetBinContent(1), " " , tmp_sel.GetBinContent(2))
-    if path==0:
-        histos['hlt']     = tmp_hlt.Clone()
-        histos['kin']     = tmp_kin.Clone()
-        histos['mva']     = tmp_mva.Clone()
-        histos['mass']    = tmp_mass.Clone()
-        histos['all_sel'] = tmp_sel.Clone()
-    else:
-        histos['hlt'].Add(tmp_hlt)
-        histos['kin'].Add(tmp_kin)
-        histos['mva'].Add(tmp_mva)
-        histos['mass'].Add(tmp_mass)
-        histos['all_sel'].Add(tmp_sel)
+dataDict = {
+    '3p7':{
+        'totalEve':55152, 
+        'path':makeFileList(commonMCPath+'HToAATo4Tau_hadronic_tauDecay_M3p7_Run3_2023/3p7_MLAnalyzer_ntuples_v1/241209_155112/0000/')
+    },
+    '14':{
+        'totalEve': 60662,
+        'path':makeFileList(commonMCPath+"HToAATo4Tau_hadronic_tauDecay_M14_Run3_2023/14_MLAnalyzer_ntuples_v1/241209_155230/0000/")
+    },
+    'QCD':{
+        'totalEve': 198000,
+        'path':makeFileList(commonMCPath+"GEN_SIM_QCD_pt15to7000_Run3Summer23GS/QCD_MLAnalyzer_ntuples_v1/241209_155310/0000/")
+    }
+}
 
-   
-print("Total events processed: " , histos['all_sel'].GetBinContent(1)+histos['all_sel'].GetBinContent(2))
-print("Trigger: ", histos['hlt'].GetBinContent(2))
-print("Kinematic cuts: ", histos['kin'].GetBinContent(2))
-print("MVA: ", histos['mva'].GetBinContent(2))
-print("Mass: ", histos['mass'].GetBinContent(2))
-print("Edge HE removed:", histos['all_sel'].GetBinContent(2))
-        
+selections = ['Trigger','Kinematic cuts','2Tau','Mass','HE edge removal']
+
+for key,value in dataDict.items():
+    print('-----------------', key, '----------------------------')
+    totalEve = value['totalEve']
+    fileList = value['path']
+    #print(f"Key: {key}, Total Events: {totalEve}, Path: {fileList}")
+    #print(key,value)
+    histos={}
+    files_ = []
+
+    for path in range(len(fileList)):
+        #print(path, fileList[path])
+        files_.append( TFile.Open(fileList[path]) )
+        tmp_hlt = files_[-1].Get('fevt/hNpassed_hlt')
+        tmp_kin = files_[-1].Get('fevt/hNpassed_kin')
+        tmp_mva = files_[-1].Get('fevt/hNpassed_2Tau')
+        tmp_mass= files_[-1].Get('fevt/hNpassed_mTT')
+        tmp_sel = files_[-1].Get('fevt/h_sel')
+        #print(path, tmp_hlt.GetBinContent(1), " " , tmp_hlt.GetBinContent(2))
+        if path==0:
+            histos['hlt']     = tmp_hlt.Clone()
+            histos['kin']     = tmp_kin.Clone()
+            histos['mva']     = tmp_mva.Clone()
+            histos['mass']    = tmp_mass.Clone()
+            histos['all_sel'] = tmp_sel.Clone()
+        else:
+            histos['hlt'].Add(tmp_hlt)
+            histos['kin'].Add(tmp_kin)
+            histos['mva'].Add(tmp_mva)
+            histos['mass'].Add(tmp_mass)
+            histos['all_sel'].Add(tmp_sel)
+    
+    tmp_hlt.Reset()
+    tmp_kin.Reset()
+    tmp_mva.Reset()
+    tmp_mass.Reset()
+    tmp_sel.Reset()
+
+    #print("Total events processed: " , histos['all_sel'].GetBinContent(1)+histos['all_sel'].GetBinContent(2))
+    print("Trigger: ", histos['hlt'].GetBinContent(2))
+    print("Kinematic cuts: ", histos['kin'].GetBinContent(2))
+    print("2Tau: ", histos['mva'].GetBinContent(2))
+    print("Mass: ", histos['mass'].GetBinContent(2))
+    print("Edge HE removed:", histos['all_sel'].GetBinContent(2))
